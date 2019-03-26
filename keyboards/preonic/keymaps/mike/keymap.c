11617f2b8bc25bcc669933850fc24926e391637a
MKennelly <mikewjkennelly@gmail.com>
2019-03-26 15:27:57 -0400

    Change shift-modded keys to the QMK keycodes

--- keyboards/preonic/keymaps/mike/keymap.c
+++ keyboards/preonic/keymaps/mike/keymap.c
@@ -23,8 +23,6 @@
 #define CTL_ESC LCTL_T(KC_ESC)
 #define ALT_TAB LALT_T(KC_TAB)
 #define SFT_GRV LSFT_T(KC_GRV)
-#define KC_LSQB S(KC_LBRC)
-#define KC_RSQB S(KC_RBRC)
 
 enum preonic_layers {
   _COLEMAK,
@@ -122,11 +120,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_F11 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,   KC_F10,    KC_F12},
-  {_______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8),    S(KC_9), S(KC_0),   _______},
-  {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,    KC_0,      _______},
-  {_______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, KC_MINS, S(KC_MINS), KC_EQL,  S(KC_EQL), _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV,    KC_MSTP, KC_MPLY,   KC_MNXT}
+  {KC_F11 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12},
+  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
+  {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  {_______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT}
 },
 
 /* Raise -  Symbols/control
@@ -144,9 +142,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = {
   {_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQL},
-  {_______, KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_END,  XXXXXXX, KC_LBRC, KC_RBRC, KC_LSQB, KC_RSQB, KC_DEL},
+  {_______, KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_END,  XXXXXXX, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_DEL},
   {_______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS},
-  {_______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, S(KC_BSLS)},
+  {_______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, KC_PIPE},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT}
 },
 
