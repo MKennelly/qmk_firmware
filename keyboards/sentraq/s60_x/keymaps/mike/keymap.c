9269d2d70903a5b1564754ff91a123131c3286ee
MKennelly <mikewjkennelly@gmail.com>
2019-04-17 16:05:36 -0400

    Update sentraq firmware to use their new layout functions, add reset key

--- keyboards/sentraq/s60_x/keymaps/mike/keymap.c
+++ keyboards/sentraq/s60_x/keymaps/mike/keymap.c
@@ -1,3 +1,5 @@
+// Can reset with either SPACE + b on plug in or RESET key
+#include QMK_KEYBOARD_H
 #include "s60_x.h"
 
 enum s60_layers {
@@ -6,10 +8,13 @@ enum s60_layers {
   _FN,
   _MOUSE,
   _ADJUST
-}
+};
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+#define FN MO(_FN)
+#define MOUSE MO(_MOUSE)
+#define ADJUST MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Colemak Mac Mods
@@ -22,15 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
 │LSHFT│▒▒▒▒▒│  Z  │  X  │  C  │  V  │  B  │  K  │  M  │  ,  │  .  │  /  │▒▒▒▒▒│RSHFT│▒▒▒▒▒│
 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-│LCTRL│L_ALT│L_CMD│█████│█████│█████│ SPC │█████│█████│█████│R_CMD│R_ALT│Mouse│ FNM │█████│
+│LCTRL│L_ALT│L_CMD│█████│█████│█████│ SPC │█████│█████│█████│R_CMD│R_ALT│Mouse│ FN  │█████│
 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
     */
-    [_COLEMAK] = KEYMAP(
-        KC_GRV,  		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, \
-        KC_TAB, 		KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,          \
-        LCTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_NO,   KC_ENT,           \
-        KC_LSFT, 		KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT, KC_NO,   \
-        KC_LCTL, 		KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT, MO(5),   MO(4)),
+    [_COLEMAK] = LAYOUT_60_ansi(
+        KC_GRV,  		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+        KC_TAB, 		KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, \
+        LCTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,  \
+        KC_LSFT, 		         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, \
+        KC_LCTL, 		KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT, MOUSE,   FN       \
+	),
 
     /* Qwerty Mac Mods
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -45,12 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 │LCTRL│L_ALT│L_CMD│█████│█████│█████│ SPC │█████│█████│█████│R_CMD│R_ALT│Mouse│ Fn0 │█████│
 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
      */
-	[_QWERTY] = KEYMAP(
-        KC_GRV,  		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, \
-        KC_TAB,  		KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          \
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,           \
-        KC_LSFT, 		KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT, KC_NO,   \
-        KC_LCTL, 		KC_LALT, KC_LGUI,                            KC_SPC,                              KC_RGUI, KC_RALT, MO(_MOUSE),   MO(_FN)),
+	[_QWERTY] = LAYOUT_60_ansi(
+        KC_GRV,  		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+        KC_TAB,  		KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
+        KC_LSFT, 		         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, \
+        KC_LCTL, 		KC_LALT, KC_LGUI,                            KC_SPC,                              KC_RGUI, KC_RALT, MOUSE,   FN       \
+	),
     /* Overlay 4: Function Layer
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │Grave│ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │▒▒▒▒▒│ Del │
@@ -64,12 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 │     │     │     │█████│█████│█████│Space│█████│█████│█████│     │     │ MO6 │     │█████│
 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
      */
-    [_FN] = KEYMAP(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL, \
-        KC_CAPS, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PGDN, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   _______, KC_INS,          \
-        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, KC_ENT,          \
-        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN, _______, _______, _______,\
-        _______, _______, _______,                            KC_SPC,                             _______, _______, MO(_ADJUST),   _______),
+    [_FN] = LAYOUT_60_ansi(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
+        KC_CAPS, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PGDN, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   _______, KC_INS, \
+        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_ENT, \
+        _______,          KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_DOWN,          _______,\
+        _______, _______, _______,                            KC_SPC,                             _______, _______, ADJUST,  _______\
+	),
     /* Overlay 5: Mouse mode (Semicolon/App)
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │Grave│ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │▒▒▒▒▒│BkSpc│
@@ -84,12 +92,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
      * Mc: Mouse Cursor / Mb: Mouse Button / Mw: Mouse Wheel
      */
-    [_MOUSE] = KEYMAP(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_BSPC, \
-        KC_TAB,  _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______,          \
-        _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, KC_ENT,           \
-        _______, _______, _______, _______, _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_BTN4, KC_BTN5, _______, _______, KC_RSFT, _______, \
-        _______, _______, _______,                            KC_BTN1,                            _______, _______, _______, KC_RCTL),
+    [_MOUSE] = LAYOUT_60_ansi(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, \
+        KC_TAB,  _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______, \
+        _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,          KC_ENT,  \
+        _______,          _______, _______, _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_BTN4, KC_BTN5, _______,          KC_RSFT, \
+        _______, _______, _______,                            KC_BTN1,                            _______, _______, _______, KC_RCTL  \
+	),
     /* Overlay 6: Layout selector
 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 │ CM  │ QW  │     │     │     │     │     │     │     │     │     │     │     │▒▒▒▒▒│RESET│
@@ -105,12 +114,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 CM: set Mac Colemak layout (0)
 CW: set Win Colemak layout (1)
     */
-    [_ADJUST] = KEYMAP(
-    TO(0),   TO(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,  \
-	_______, TO(2),   TO(3),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         \
-	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         \
-	_______, _______, _______, _______, TO(1),   _______, _______, _______, TO(0),   _______, _______, _______, _______, _______, _______,\
-	_______, _______, _______,                            _______,                   _______, _______, _______, _______),
+    [_ADJUST] = LAYOUT_60_ansi(
+    TO(0),   TO(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,  \
+	_______, TO(2),   TO(3),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+	_______,          _______, _______, TO(1),   _______, _______, _______, TO(0),   _______, _______, _______,          _______, \
+	_______, _______, _______,                            _______,                            _______, _______, _______, _______  \
+	),
 };
 
 /*
